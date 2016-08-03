9da180427a65ea6f956396a06c45716c2d41dbd1
Amit Khandelwal <amitmbm87@gmail.com>
2016-08-03 22:37:17 +0530

    Heath check added.

---
+++ src/main/java/com/ami/resources/HeathCheckController.java
@@ -0,0 +1,24 @@
+package com.ami.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by amit.khandelwal on 03/08/16.
+ */
+
+@Path("/status")
+public class HeathCheckController {
+
+    @GET
+    public Response getHeathCheck() {
+        System.out.println("inside health check controller");
+        return Response.status(200).entity("server is fine").build();
+    }
+
+}
+
+
